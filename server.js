dc864d5e95eaac74f86b34d1747147718c584be2
Joanna Preston <joannappreston@gmail.com>
2020-06-26 13:56:31 -0700

    initial commit - file structure and html routes working

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// set up port and express app
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// set up logger
+app.use(logger("dev"));
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// set up mongodb connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", { useNewUrlParser: true });
+
+// require routes
+// app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`);
+});
